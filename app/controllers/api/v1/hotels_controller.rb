fde41d03e7d26b3ce6ec1ace73e3491cbe6530ec
pwambui93@gmail.com <pwambui93@gmail.com>
2021-03-17 17:17:25 +0300

    Finish hotel controller

--- app/controllers/api/v1/hotels_controller.rb
+++ app/controllers/api/v1/hotels_controller.rb
@@ -2,36 +2,24 @@ class Api::V1::HotelsController < ApplicationController
 
     def index
         hotel = Hotel.all
-
         render json: {
-
               data: hotel,
               status: :ok
-
            }
-
     end
 
     def show
-
         hotel = Hotel.find( params[:id] )
-
         render json: {
-
               data: hotel,
-            #   images: @avatars,
               status: :ok
-
-           }
-        
+           }  
     end
     
 
     private
     def params_hotel
-
-    params.require(:Hotel).permit(:name, :description, :price, :location, :images)
-        
+    params.require(:Hotel).permit(:name, :description, :price, :location, :images)     
     end
 
 end
