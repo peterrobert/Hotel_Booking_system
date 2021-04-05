dac3ace43619d712b9110a08cb8b6e225976797f
pwambui93@gmail.com <pwambui93@gmail.com>
2021-04-05 08:43:15 +0300

    Add database to production

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   namespace :api do
     namespace :v1 do
       resources :hotels, only: [:index, :show]
