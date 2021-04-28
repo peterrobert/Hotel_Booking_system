1ddd1932890ac2f55ee9e86ec9f09c5fc196a838
pwambui93@gmail.com <pwambui93@gmail.com>
2021-04-28 10:56:50 +0300

    Fix unnecessary device functionalities

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   acts_as_token_authenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable
 
   has_many :bookings
   has_many :hotels, through: :bookings
